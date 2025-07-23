efe44930d5183f6a5a7356405949ead1978a4216
Dinesh Dakhare <dineshdakhare22@gmail.com>
2025-07-23 18:56:27 +0530

    hero seaction is done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Hero from './components/Hero.jsx'
+function App() {
+
+
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+<Hero/>
+    </main>
+   
+  )
+}
+
+export default App
